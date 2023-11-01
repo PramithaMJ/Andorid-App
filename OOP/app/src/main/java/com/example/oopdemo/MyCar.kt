6ba5b0c1c0c9c7c37b0513a21677991d1b2901b4
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-11-01 21:08:04 +0530

    sp

--- OOP/app/src/main/java/com/example/oopdemo/MyCar.kt
+++ OOP/app/src/main/java/com/example/oopdemo/MyCar.kt
@@ -2,8 +2,16 @@ package com.example.oopdemo
 
 import android.util.Log
 
-class MyCar : Car() {
+class MyCar : Car(), SpeedController {
     override fun start() {
-        Log.i("MYTAG","this is Mycar start...")
+        Log.i("MYTAG","this is Mycar starting... BrandId id ${getBrandID()}")
+    }
+
+    override fun accelaration() {
+        TODO("Not yet implemented")
+    }
+
+    override fun deaccelaration() {
+        TODO("Not yet implemented")
     }
 }
