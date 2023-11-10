d6ca36ebf69187711a3757d9abd1ea7852fc04b9
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-11-10 17:21:51 +0530

    encapsulated live data

--- ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
+++ ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewmodeldemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodeldemo2.databinding.ActivityMainBinding
 
@@ -16,12 +17,16 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         // get view model object
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainActivityViewModel::class.java)
-
-        binding.resultTextView.text = viewModel.getTotal().toString()
+        // with liveData
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTextView.text = it.toString()
+        })
+        // without liveData
+      //  binding.resultTextView.text = viewModel.getTotal().toString()
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text =viewModel.getTotal().toString()
+            //binding.resultTextView.text =viewModel.getTotal().toString() // no need this liveData automaically update
             binding.inputEditText.text.clear()
         }
     }
--- ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivityViewModel.kt
+++ ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,19 +1,25 @@
 package com.example.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal : Int) : ViewModel(){
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData : LiveData<Int>
+        get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
-    fun getTotal():Int{
-        return total
-    }
+    // don't need this function because live data will automatically observed data
+//    fun getTotal():Int{
+//        return total
+//    }
 
     fun setTotal(input:Int){
-        total +=input
+        //total +=input // without using liveData
+        total.value = (total.value)?.plus(input) // when using liveData
     }
 }
