6134caad0758a61e00246207acbfff19ba172010
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-11-10 16:00:03 +0530

    ViewModel

---
+++ ViewModelDemo2/app/src/main/java/com/example/viewmodeldemo2/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.viewmodeldemo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodeldemo2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        // get view model object
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.resultTextView.text = viewModel.getTotal().toString()
+
+        binding.insertButton.setOnClickListener {
+            viewModel.setTotal(binding.inputEditText.text.toString().toInt())
+            binding.resultTextView.text =viewModel.getTotal().toString()
+            // clear the input text
+            binding.inputEditText.text = viewModel.
+        }
+    }
+}
